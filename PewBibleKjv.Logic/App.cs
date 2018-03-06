8ca64ccfee9fa02c8709eb3598c01b817909f04d
Stephen Cleary <StephenCleary@users.noreply.github.com>
2018-03-05 20:30:15 -0800

    Reducing code.

--- PewBibleKjv.Logic/App.cs
+++ PewBibleKjv.Logic/App.cs
@@ -6,21 +6,14 @@ using PewBibleKjv.Text;
 
 namespace PewBibleKjv.Logic
 {
-    public sealed class App : IDisposable
+    public sealed class App
     {
         private readonly IVerseView _verseView;
 
         public App(IVerseView verseView)
         {
             _verseView = verseView;
-
-            // Keep track of changes to the verse view.
             verseView.OnSwipeLeft += MoveNextChapter;
-        }
-
-        public void Dispose()
-        {
-            _verseView.OnSwipeLeft -= MoveNextChapter;
         }
 
         private void MoveNextChapter()
